6bbd4fa9622206e76e4d517af79bc6fef300da8d
Ben Montgomery <ben_mon@outlook.com>
2019-04-21 10:23:08 -0400

    Basic laser and detector tests

--- tests/detector_test.py
+++ tests/detector_test.py
@@ -4,11 +4,52 @@ import numpy as np
 from quadrantdetector import detector
 
 
+axis_size = 1000
+
+
 @pytest.fixture(scope='session')
 def get_detectors():
     """
     Returns a list of 200 detectors with increasingly large gaps, the last
     being gaps larger than the actual detector.
     """
-    return [(gap, detector.create_detector(1000, 20, 1e-12))
-            for gap in np.linspace(1e-14, 21, 200)]
+    return [(gap, detector.create_detector(axis_size, 10, gap))
+            for gap in np.linspace(1e-14, 1, 50)] \
+        + [(gap, detector.create_detector(axis_size, 10, gap))
+            for gap in np.linspace(1, 11, 50)]
+
+
+def test_detector_init(get_detectors):
+    for gap, detect in get_detectors:
+        assert detect.shape == (axis_size, axis_size)
+
+        axis_0_zeros = 0
+        axis_1_zeros = 0
+        density = axis_size / 10
+        inset = min(int((axis_size // 2) + (gap * density // 2) + 2), axis_size - 1)
+        # Use the fact that the detector is NxN for easier checking.
+        for i in range(len(detect)):
+            # On each gap, there is a minimum of gap - 2 cells on the detector
+            # that are 0.
+            # The cases are : gap - 2 of 0s, 2 values on either side < 1.
+            #                 gap of 0s.
+            # We also have to deal with the zeroed out borders of the detector.
+
+            if not detect[i, inset]:
+                axis_0_zeros += 1
+
+            if not detect[inset, i]:
+                axis_1_zeros += 1
+
+        assert axis_0_zeros >= min(gap * density - 2, axis_size)
+        assert axis_1_zeros >= min(gap * density - 2, axis_size)
+
+def test_laser():
+    # Test a laser on an empty grid.
+    # On our 1000x1000 grid @ 10mm, the sum of a guassian beam is 10k.
+    # In real life, we have a finite grid, so > 9999.
+    # Pick a bound for sigma where the majority is visibly on the grid.
+    for sigma in np.linspace(1e-2, 1, 50):
+        grid = np.ones([1000, 1000])
+        laser = detector.laser(grid, 10/1000, 0, 0, sigma)
+        assert laser.sum() > 9999
