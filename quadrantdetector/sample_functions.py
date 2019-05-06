7963cb52c530b93fc01262f45b04ebc3f34b5226
Ben Montgomery <ben_mon@outlook.com>
2019-05-06 15:59:29 -0400

    imports fix

--- quadrantdetector/sample_functions.py
+++ quadrantdetector/sample_functions.py
@@ -16,7 +16,7 @@ def periodogram_psd(v, fs):
     return f, psd
 
 
-def SIN_PATH(t, d0):
+def sin_path(t, d0):
     """
     Describes the y values of a sine wave given a "position" t and detector
     diameter d0.
@@ -25,13 +25,28 @@ def SIN_PATH(t, d0):
     return 0.5 * d0 * np.sin(2 * np.pi * t / 2.0)
 
 
-def CENTER_PATH(x, d0):
+def center_path(x, d0):
+    """
+    Describes the y values of a a straight path across the center of a
+    detector given a position x and detector diameter d0.
+
+    """
     return 0.0
 
 
-def HALF_PATH(x, d0):
+def half_path(x, d0):
+    """
+    Describes the y values of a a straight path halfway above the center of a
+    detector given a position x and detector diameter d0.
+
+    """
     return d0 / 4
 
 
-def QUARTER_PATH(x, d0):
+def quarter_path(x, d0):
+    """
+    Describes the y values of a a straight path a quarter above the center of a
+    detector given a position x and detector diameter d0.
+
+    """
     return d0 / 8
