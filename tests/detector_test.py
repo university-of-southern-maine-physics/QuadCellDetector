f9cf5ecf112d90a29c72b439403c592771ee8b30
Ben Montgomery <ben_mon@outlook.com>
2019-05-07 13:36:16 -0400

    Updated unit tests

--- tests/detector_test.py
+++ tests/detector_test.py
@@ -43,21 +43,26 @@ def test_detector_init(get_detectors):
         assert axis_1_zeros >= min(gap * density - 2, axis_size)
 
 
-def test_laser():
+def test_laser(get_detectors):
     # Test a laser on an empty grid.
-    # On our 1000x1000 grid @ 10mm, the sum of a Guassian beam is 10k.
-    # In real life, we have a finite grid, so > 9999.
-    # Pick a bound for sigma where the majority is visibly on the grid.
-    for sigma in np.linspace(1e-2, 1, 50):
-        grid = np.ones([1000, 1000])
-        laser = detector.laser(grid, 10 / axis_size, 0, 0, sigma)
-        assert 10001 > laser.sum() > 9999
+    # In every laser on a detector, the sum of every point should be
+    # approximately 1 when sigma << detector diameter.
+    # Clearly, this only holds when the gap is small, otherwise the sum will
+    # be even smaller.
+    for gap, detect in get_detectors:
+        laser = detector.laser(detect, 10 / axis_size, 0, 0, 1.0)
+        sum_s = np.sum(laser * detect)
+
+        if gap < 1e-2:
+            assert 1 >= sum_s > 0.99
+        if gap > 1:
+            assert 0.99 >= sum_s >= 0
 
 
 def test_compute_signals(get_detectors):
     for gap, detect in get_detectors:
         # get_detectors created a sequence of detectors centered, so all
         # signals should be symmetric.
-        sum_signal, lr_signal, tb_signal = detector.compute_signals(detector.laser(detect, 10 / axis_size, 0, 0, 2.0),
-                                                                    detect)
+        sum_signal, lr_signal, tb_signal = detector.compute_signals(
+            detector.laser(detect, 10 / axis_size, 0, 0, 2.0), detect)
         assert sum_signal >= lr_signal and sum_signal >= tb_signal
