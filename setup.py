67cb21c862460e9edc0f6d44076250ad72f1c7d3
Ben Montgomery <ben_mon@outlook.com>
2019-06-25 11:32:08 -0400

    Switched to PBR versioning

--- setup.py
+++ setup.py
@@ -1,29 +1,3 @@
-from setuptools import setup
+import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setup(name='quadcelldetector',
-      description="A library to simulate the electronic output resulting from"
-      "the passage of a gaussian beam over a quadrant cell photodiode",
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      version='0.1',
-      python_requires='>=3.5.6',
-      url='https://github.com/paulnakroshis/QuadCellDetector',
-      author='Paul Nakroshis',
-      author_email="author@example.com",
-      packages=['quadrantdetector'],
-      licence='BSD 3-Clause License',
-      classifiers=[
-          'Development Status :: 4 - Beta',
-          'Intended Audience :: Science/Research',
-          'License :: OSI Approved :: BSD 3-Clause License',
-          'Programming Language :: Python :: 3'
-      ],
-      install_requires=[
-                        'scipy',
-                        'numpy',
-                        'matplotlib'
-                       ],
-      zip_safe=False)
+setuptools.setup(setup_requires=['pbr'], pbr=True)
